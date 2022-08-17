5598c9618e2733f2f8a3635243999cf5d507075a
Filip Mlynek <mlynekff@gmail.com>
2022-08-17 17:58:21 +0200

    Added logging message

--- src/main/java/com/filip/managementapp/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/filip/managementapp/security/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,8 @@
 package com.filip.managementapp.security;
 
+import com.filip.managementapp.controller.ApiExceptionControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,8 +15,10 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApiExceptionControllerAdvice.class);
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Unauthorized exception: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
