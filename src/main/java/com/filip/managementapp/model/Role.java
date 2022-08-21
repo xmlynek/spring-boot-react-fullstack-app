70389b8e6689f4b9d16dfd5d23da2b14857bab44
Filip Mlynek <mlynekff@gmail.com>
2022-08-21 22:46:17 +0200

    modified generation strategy

--- src/main/java/com/filip/managementapp/model/Role.java
+++ src/main/java/com/filip/managementapp/model/Role.java
@@ -17,7 +17,7 @@ import java.util.Set;
 @AllArgsConstructor
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
