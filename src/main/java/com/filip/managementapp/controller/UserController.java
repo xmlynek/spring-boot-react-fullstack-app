b5966f569c2dd8c91f27a6bbd6b414f480f6911d
Filip Mlynek <mlynekff@gmail.com>
2022-08-23 14:30:27 +0200

    reduce users api

--- src/main/java/com/filip/managementapp/controller/UserController.java
+++ src/main/java/com/filip/managementapp/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.filip.managementapp.controller;
 
 import com.filip.managementapp.dto.UserDto;
-import com.filip.managementapp.dto.UserRequest;
 import com.filip.managementapp.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -24,33 +23,5 @@ public class UserController {
         return userService.getCurrentlyLoggedUser(principal);
     }
 
-    @GetMapping
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    public List<UserDto> findAllUsers() {
-        return userService.findAllUsers();
-    }
 
-    @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    public UserDto getUserById(@PathVariable("id") Long id) {
-        return userService.findUserById(id);
-    }
-
-    @PostMapping
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    public UserDto createUser(@RequestBody @Valid UserRequest userRequest) {
-        return userService.createUser(userRequest);
-    }
-
-    @PutMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    public UserDto updateUser(@PathVariable("id") Long id, @RequestBody @Valid UserRequest userRequest) {
-        return userService.updateUser(id, userRequest);
-    }
-
-    @DeleteMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    public void deleteUser(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
-    }
 }
