0c669b733d3cb29f852a6ac638ab6a1c032d44cf
Filip Mlynek <mlynekff@gmail.com>
2022-08-24 16:37:50 +0200

    Modified react auth stuff

--- src/frontend/src/components/pages/UserProfile.jsx
+++ src/frontend/src/components/pages/UserProfile.jsx
@@ -1,8 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 
 const UserProfile = () => {
   const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate('/login');
+    }
+  }, [currentUser, navigate]);
 
   return <>{JSON.stringify(currentUser)}</>;
 };
