8b9b4edf4354572adf8641d4e107d6ee148ac5f9
Filip Mlynek <mlynekff@gmail.com>
2022-09-01 23:51:22 +0200

    Users API prototype

--- src/main/java/com/filip/managementapp/controller/UserController.java
+++ src/main/java/com/filip/managementapp/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.filip.managementapp.controller;
 
 import com.filip.managementapp.dto.UserDto;
+import com.filip.managementapp.dto.UserRequest;
 import com.filip.managementapp.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -23,5 +25,36 @@ public class UserController {
         return userService.getCurrentlyLoggedUser(principal);
     }
 
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    public List<UserDto> findAllUsers() {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    public UserDto findUserById(@PathVariable("id") Long id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto createUser(@RequestBody @Valid UserRequest userRequest) {
+        return userService.createUser(userRequest);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    public UserDto updateUser(@PathVariable("id") Long id, @RequestBody @Valid UserDto userDtoRequest) {
+        return userService.updateUser(id, userDtoRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+    }
 
 }
