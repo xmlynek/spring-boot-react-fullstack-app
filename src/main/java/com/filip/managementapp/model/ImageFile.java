3ee50cddc753c25ff09bd32f8cbb2ec0bc7fd548
Filip Mlýnek <mlynekff@gmail.com>
2022-09-21 20:40:44 +0200

    Updated column constraints and serialVersionUID

--- src/main/java/com/filip/managementapp/model/ImageFile.java
+++ src/main/java/com/filip/managementapp/model/ImageFile.java
@@ -17,16 +17,19 @@ import java.io.Serializable;
 public class ImageFile implements Serializable {
 
     @Serial
-    private static final long serialVersionUID = 182272432111312L;
+    private static final long serialVersionUID = 177242432131615L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String filename;
 
+    @Column(nullable = false)
     private String contentType;
 
+    @Column(nullable = false)
     @Lob
     private byte[] data;
 }
